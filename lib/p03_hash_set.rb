69b9ec10af40e561bc79d9eac1e890f712ce2e47
Robert Neal <robertwneal2@gmail.com>
2022-01-31 11:04:05 -0800

    made hash set

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -7,18 +7,35 @@ class HashSet
   end
 
   def insert(key)
+    if !self.include?(key)
+      self[key].push(key)
+      @count += 1
+      if @count > num_buckets
+        resize!
+      end
+      true
+    else 
+      false
+    end
   end
 
   def include?(key)
+    return self[key].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
-  def [](num)
+  def [](val)
     # optional but useful; return the bucket corresponding to `num`
+    hash_val = val.hash
+    @store[hash_val % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +43,10 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    new_num_buckets = num_buckets * 2
+    @store = Array.new(new_num_buckets) { Array.new }
+    old_store.flatten.each { |key| self.insert(key) }
   end
 end
