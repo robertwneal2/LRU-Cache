e7054e81ed125d2d5a60212ad571ba0ee5623aef
Robert Neal <robertwneal2@gmail.com>
2022-01-31 19:24:54 -0800

    created linked list

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -14,13 +14,24 @@ class Node
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
+    self.next = nil
+    self.prev = nil
   end
 end
 
 class LinkedList
+
+  include Enumerable
+
+  attr_reader :head, :tail
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +40,73 @@ class LinkedList
   end
 
   def first
+    empty? ? nil : @head.next
   end
 
   def last
+    empty? ? nil : @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node|
+      if node.key == key
+        return node.val
+      end
+    end
+    nil
   end
 
   def include?(key)
+    self.each do |node|
+      if node.key == key
+        return true
+      end
+    end
+    false
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    node.prev = @tail.prev
+    node.next = @tail
+    @tail.prev.next = node
+    @tail.prev = node
+    node
   end
 
   def update(key, val)
+    self.each do |node|
+      if node.key == key
+        node.val = val
+        return node
+      end
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        node.remove
+        return node.val
+      end
+    end
+    nil
   end
 
   def each
+    current_node = @head.next
+    until current_node == @tail
+      yield current_node
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
